e51e8b24db9eeb453b9c77615917bb69c5251c62
Tyler Benfield <rtbenfield@gmail.com>
2015-05-26 14:08:57 -0400

    Missed a file in the last commit

---
+++ Mailgun/MailgunWebhook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Mailgun.Internal;
+
+namespace Mailgun
+{
+    public class MailgunWebhook
+    {
+        public MailgunWebhookType Type { get; set; }
+
+        public string Url { get; set; }
+
+
+        internal IEnumerable<KeyValuePair<string, string>> ToKeyValuePair()
+        {
+            var ret = new Dictionary<string, string>();
+            ret.Add("id", Utilities.GetEnumStringValue(Type));
+            ret.Add("url", Url);
+            return ret;
+        }
+    }
+}
